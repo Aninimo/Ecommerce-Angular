7a38f975c3517ef7ee17505ce1931ceb45d5415b
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-02-20 22:50:03 -0200

    Update productController.js

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,9 +1,10 @@
-
 const products = [
-    { id: 1, name: 'Pacakes and Mapão Syrup', price: 15.00, image: 'https://images.unsplash.com/photo-1551248429-40975aa4de74?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fFNhbGFkfGVufDB8fDB8fHww' },
-    { id: 2, name: 'Plain toast & Butter or Jam', price: 28.00, image: 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FsYWR8ZW58MHx8MHx8fDA%3D' },
-    { id: 3, name: 'Hashbrows ARA Potato Pancakes', price: 50.00, image: 'https://images.unsplash.com/photo-1607532941433-304659e8198a?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2FsYWR8ZW58MHx8MHx8fDA%3D' },
-    { id: 4, name: 'Tasted English Muftim', price: 25.00, image: 'https://images.unsplash.com/photo-1604909054103-f9ed51a70caf?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzB8fHNhbGFkfGVufDB8fDB8fHww'}
+  { id: 1, name: 'Pacakes and Mapão Syrup', price: 15.00, image: 'https://images.unsplash.com/photo-1551248429-40975aa4de74?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fFNhbGFkfGVufDB8fDB8fHww' },
+  { id: 2, name: 'Plain toast & Butter or Jam', price: 28.00, image: 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FsYWR8ZW58MHx8MHx8fDA%3D' },
+  { id: 3, name: 'Hashbrows ARA Potato Pancakes', price: 50.00, image: 'https://images.unsplash.com/photo-1607532941433-304659e8198a?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2FsYWR8ZW58MHx8MHx8fDA%3D' },
+  { id: 4, name: 'Tasted English Muftim', price: 25.00, image: 'https://images.unsplash.com/photo-1604909054103-f9ed51a70caf?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzB8fHNhbGFkfGVufDB8fDB8fHww'},
+  { id: 5, name: 'Baget with cream cheese', price: 30.75, image: 'https://images.unsplash.com/photo-1599021419847-d8a7a6aba5b4?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fFNhbGFkfGVufDB8fDB8fHww' },
+  { id: 6, name: 'Sausage and Egg Sandwich', price: 27.80, image: 'https://plus.unsplash.com/premium_photo-1673590981810-894dadc93a6d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODF8fFNhbGFkfGVufDB8fDB8fHww'}
 ];
 
 function getProducts(req, res) {
